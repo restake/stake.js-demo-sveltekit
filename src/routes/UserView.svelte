e2f366864213a04665c7a731bd6b7b8b74a74db5
Mark Vainomaa <mikroskeem@mikroskeem.eu>
2023-05-09 20:07:51 +0300

    Implement eth_sign testing

--- src/routes/UserView.svelte
+++ src/routes/UserView.svelte
@@ -8,6 +8,8 @@
     let chainId = useChainId(provider);
     let connectedAddress = useAccount(provider);
 
+    let lastSignedData: string | undefined = undefined;
+
     function resolveChainName(chainId: string | undefined): string {
         if (!chainId) {
             return "unknown";
@@ -32,6 +34,20 @@
             });
     }
 
+    async function signExample() {
+        lastSignedData = undefined;
+        const result: string = await interceptError(
+            provider.request({
+                method: "eth_sign",
+                params: [
+                    $connectedAddress,
+                    "0x1c8aff950685c2ed4bc3174f3472287b56d9517b9c948127319a09a7a36deac8",
+                ],
+            })
+        );
+        lastSignedData = result;
+    }
+
     $: console.log("RPC connection ready", $connected);
     $: console.log("Using network", $chainId);
     $: console.log("Using account", $connectedAddress);
@@ -47,9 +63,17 @@
     {#if !$connected}
         MetaMask RPC connection is not ready
     {:else if $connectedAddress}
-        connected to <pre>{$connectedAddress}</pre>
-        via chain
-        <pre>{resolveChainName($chainId)}</pre>
+        <div>
+            connected to <pre>{$connectedAddress}</pre>
+            via chain
+            <pre>{resolveChainName($chainId)}</pre>
+        </div>
+        <div>
+            <button on:click={() => signExample()}>Sign raw data</button>
+            {#if lastSignedData}
+                <pre>{lastSignedData}</pre>
+            {/if}
+        </div>
     {:else}
         <button on:click={() => connect()}>Connect</button>
     {/if}
